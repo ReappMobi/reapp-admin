a7b0a3f74ada160838cf11acae20c85d5e8d30fa
André Gabriel <andreon098@gmail.com>
2025-03-03 09:56:46 -0300

    refactor: reformat files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Page() {
-  return (
-    <h1 className="text-center text-2xl font-bold">Reapp</h1>
-  );
+  return <h1 className="text-center text-2xl font-bold">Reapp</h1>;
 }
