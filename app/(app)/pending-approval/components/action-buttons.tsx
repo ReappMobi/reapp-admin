623632482ad216f100fea67bab11d486c2a689c4
André Gabriel <andreon098@gmail.com>
2025-03-08 10:35:09 -0300

    feat: add ActionButtons component for approving and rejecting actions

---
+++ app/(app)/pending-approval/components/action-buttons.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { useState } from 'react';
+
+type ActionValue = 'approve' | 'reject';
+
+export function ActionButtons() {
+  const [value, setValue] = useState<ActionValue>();
+  return (
+    <>
+      <input type="hidden" name="action" value={value} />
+      <DropdownMenuItem className="cursor-pointer" asChild>
+        <Button variant={'ghost'} onClick={() => setValue('approve')}>
+          Aprovar
+        </Button>
+      </DropdownMenuItem>
+      <DropdownMenuItem className="cursor-pointer" asChild>
+        <Button variant="ghost" onClick={() => setValue('reject')}>
+          Recusar
+        </Button>
+      </DropdownMenuItem>
+    </>
+  );
+}
