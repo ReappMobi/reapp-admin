7245232fb4f3ab852ec7dfc3fea376ac8dd2570d
André Gabriel <andreon098@gmail.com>
2025-03-08 10:42:44 -0300

    feat: update backend functions to return typed responses for pending institutions

--- lib/backend.ts
+++ lib/backend.ts
@@ -1,36 +1,39 @@
+import type { GetPendingApprovalInstitution } from '@/types/institution';
+
 const backendUrl = process.env.API_URL || 'http://localhost:3000';
 
-async function getPendingInstitutions(token: string) {
-  const data = await fetch(`${backendUrl}/account/institutions/pending`, {
+async function getPendingInstitutions(
+  token: string,
+): Promise<GetPendingApprovalInstitution[] | undefined> {
+  const response = await fetch(`${backendUrl}/account/institutions/pending`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
   });
-  return data;
+  const institutions = await response.json();
+  return institutions;
 }
 
-async function approveInstitution(token: string, id: number) {
-  const data = await fetch(`${backendUrl}/account/institution/approve/${id}`, {
+async function approveInstitution(token: string, id: number): Promise<void> {
+  await fetch(`${backendUrl}/account/institution/approve/${id}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
   });
-  return data;
 }
 
-async function rejectInstitution(token: string, id: number) {
-  const data = await fetch(`${backendUrl}/account/institution/reject/${id}`, {
+async function rejectInstitution(token: string, id: number): Promise<void> {
+  await fetch(`${backendUrl}/account/institution/reject/${id}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
   });
-  return data;
 }
 
 export default {
