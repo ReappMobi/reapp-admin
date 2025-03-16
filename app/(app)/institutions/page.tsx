9b40ec0fd645e1f1f7e77037cbcf866d78f94da5
André Gabriel <andreon098@gmail.com>
2025-03-16 11:07:45 -0300

    feat: filter out pending accounts from institutions list

--- app/(app)/institutions/page.tsx
+++ app/(app)/institutions/page.tsx
@@ -8,17 +8,20 @@ import {
 } from '@/components/ui/card';
 
 import { getInstitutions } from '@/lib/backend';
+import { AccountStatus } from '@/types/account';
 import { columns } from './components/columns';
 import { DataTable } from './components/data-table';
 
 export default async function Page() {
-  const institutions = await getInstitutions();
+  const institutions = (await getInstitutions()).filter(
+    (account) => account.status !== AccountStatus.PENDING,
+  );
 
   return (
-    <div className="container mx-auto h-screen max-w-screen-lg p-10">
+    <div className="container mx-auto h-full max-w-screen-lg">
       <Card
         x-chunk="dashboard-06-chunk-0"
-        className="h-full w-full flex-1 md:max-w-screen-lg"
+        className="h-full w-full flex-1 border-0 shadow-none"
       >
         <CardHeader>
           <CardTitle className="text-2xl">Instituições</CardTitle>
